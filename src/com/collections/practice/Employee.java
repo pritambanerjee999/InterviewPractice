b9b2de768be53037689dfe172b8152436672b443
Pritam Banerjee <pritambanerjee999@gmail.com>
2017-03-16 04:46:31 -0700

    Latest message

---
+++ src/com/collections/practice/Employee.java
@@ -0,0 +1,78 @@
+package com.collections.practice;
+
+import java.util.Comparator;
+
+public class Employee implements Comparable {
+	
+	private Integer id;
+	private String fullName;
+	private Integer age;
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (fullName == null) {
+			if (other.fullName != null)
+				return false;
+		} else if (!fullName.equals(other.fullName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
---
+++ src/com/simple/problems/ReverseStringUsingRecursion.java
@@ -0,0 +1,16 @@
+package com.simple.problems;
+
+public class ReverseStringUsingRecursion {
+
+//	public static void main(String args[]){
+//		String str = "testing";
+//		System.out.println(reverseStr(str));
+//	}
+//	
+//	public static String reverseStr(String str){
+//		char arr[] = str.toCharArray();
+//		return String.copyValueOf(reverse(arr));
+//	}
+//	
+	
+}
